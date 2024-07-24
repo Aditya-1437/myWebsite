4a33316f0015726ab61622cc80d664c3cdb8e133
Aditya.K <kunchaaditya369@gmail.com>
2024-07-24 16:27:33 +0530

    added complete naviagtion toggles

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display = 'flex';
-}
-function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display = 'none';
+function toggleMenu(){
+    const menu = document.querySelector('.menu-links')
+    const icon = document.querySelector('.toggle-icon')
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
 }
